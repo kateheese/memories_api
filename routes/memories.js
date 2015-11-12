caf827e9400b0ab6b4e3387345b4c1b77e33506d
kateheese <kateheese@gmail.com>
2015-11-12 15:21:26 -0700

    users can create a new memory

--- routes/memories.js
+++ routes/memories.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = process.env.DATABASE_URL;
+var conString = 'postgres://@localhost/memoriesapp';
 
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
 
-});
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1, $2, $3)',[req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
+      done();
+      res.status(200).end();
+      if (err) {
+        return console.error('error running query', err);
+      }
+    });
+  });
+})
 
 module.exports = router;
